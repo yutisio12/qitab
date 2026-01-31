6500d67b4d7021faefd938fe44652d8f9fc116c8
yutisio12 <yutisiogamings@gmail.com>
2026-01-31 17:12:14 +0700

    FE: - add toast on template - add cofnirmDialog on template

--- app.vue
+++ app.vue
@@ -1,3 +1,40 @@
 <template>
+  <Toast 
+    :pt="{
+      root: 'p-toast',
+      message: 'p-toast-message',
+      icon: 'p-toast-icon',
+      summary: 'p-toast-summary',
+      detail: 'p-toast-detail',
+      closeButton: 'p-toast-icon-close',
+      closeIcon: 'pi pi-times',
+    }" 
+    style="width: 25%;"
+  />
+  <ConfirmDialog 
+    :pt="{
+      root: 'p-confirm-dialog',
+      header: 'p-confirm-dialog-header',
+      title: 'p-confirm-dialog-title',
+      icon: 'p-confirm-dialog-icon',
+      content: 'p-confirm-dialog-content',
+      message: 'p-confirm-dialog-message',
+      footer: 'p-confirm-dialog-footer',
+      mask: 'p-dialog-mask',
+      pcRejectButton: {
+        root: 'p-confirm-dialog-reject'
+      },
+      pcAcceptButton: {
+        root: 'p-confirm-dialog-accept'
+      },
+      rejectButton: {
+        root: 'p-confirm-dialog-reject'
+      },
+      acceptButton: {
+        root: 'p-confirm-dialog-accept'
+      }
+    }"
+    style="text-align: center;"
+  ></ConfirmDialog>
   <NuxtPage />
 </template>
