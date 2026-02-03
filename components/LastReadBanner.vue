9222c84d7a09ba05da0af0ea461e017a32095e36
yutisio12 <yutisiogamings@gmail.com>
2026-02-03 19:32:30 +0700

    FE: - Rectifying LastReadBanner - Rectifying SurahListItem

--- components/LastReadBanner.vue
+++ components/LastReadBanner.vue
@@ -5,9 +5,10 @@
 <template>
   <div 
     v-if="lastRead"
-    class="mb-6 p-4 border rounded-lg bg-yellow-50 flex justify-between items-center"
+    class="mb-6 p-4 border bg-yellow-100 flex justify-between items-center"
+    style="border-bottom-right-radius: 10px; border-bottom-left-radius: 10px;"
   >
-    <p class="text-sm text-gray-600">Terakhir Dibaca: </p>
+    <p class="text-sm text-gray-600 text-semibold">Terakhir Dibaca: </p>
     <NuxtLink :to="`/surah/${lastRead.surahId}#ayat-${lastRead.ayat}`"
       class="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600">
       {{ lastRead.surahName }} - {{ lastRead.ayat }}
--- pages/index.vue
+++ pages/index.vue
@@ -20,15 +20,28 @@
       Daftar Surah Al-Qur’an
     </h1>
 
-    <div v-if="pending">Loading...</div>
+    <div v-if="pending">
+      <Skeleton width="100%" height="4rem" class="p-4 mb-2"></Skeleton>
+      <Skeleton width="100%" height="4rem" class="p-4 mb-2"></Skeleton>
+      <Skeleton width="100%" height="4rem" class="p-4 mb-2"></Skeleton>
+      <Skeleton width="100%" height="4rem" class="p-4 mb-2"></Skeleton>
+      <Skeleton width="100%" height="4rem" class="p-4 mb-2"></Skeleton>
+      <Skeleton width="100%" height="4rem" class="p-4 mb-2"></Skeleton>
+      <Skeleton width="100%" height="4rem" class="p-4 mb-2"></Skeleton>
+      <Skeleton width="100%" height="4rem" class="p-4 mb-2"></Skeleton>
+      <Skeleton width="100%" height="4rem" class="p-4 mb-2"></Skeleton>
+    </div>
     <div v-else-if="error">Gagal memuat data</div>
 
     <ul v-else>
-      <SurahListItem 
-        v-for="surah in surahList" 
-        :key="surah.nomor" 
-        :surah="surah" 
-      />
+      <ScrollTop />
+      <ScrollPanel class="shadow-md" style="width: 100%; height: 750px; padding: 15px;">
+        <SurahListItem 
+          v-for="surah in surahList" 
+          :key="surah.nomor" 
+          :surah="surah" 
+        />
+      </ScrollPanel>
     </ul>
   </div>
 </template>
