84c7680e290fc802e2356ce4e35abe206603aa4d
yutisio12 <yutisiogamings@gmail.com>
2026-02-12 21:02:44 +0700

    FE: - install colorMode - setup colorMode

---
+++ components/ColorModeSelector.vue
@@ -0,0 +1,27 @@
+<script setup>
+const colorMode = useColorMode()
+
+const isDark = computed({
+  get() {
+    return colorMode.value === 'dark'
+  },
+  set(value) {
+    colorMode.preference = value ? 'dark' : 'light'
+  }
+})
+
+const toggleColorMode = () => {
+  colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+}
+</script>
+
+<template>
+  <button
+    @click="toggleColorMode"
+    class="relative inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-100 dark:bg-gray-800 text-gray-700 dark:text-gray-300 hover:ring-2 hover:ring-primary-500 transition-all duration-300 focus:outline-none"
+    aria-label="Toggle Dark Mode"
+  >
+    <i v-if="colorMode.value === 'dark'" class="pi pi-moon text-lg"></i>
+    <i v-else class="pi pi-sun text-lg"></i>
+  </button>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="fixed bottom-6 right-6 z-[100]">
+    <ColorModeSelector />
+  </div>
+  <slot />
+</template>
