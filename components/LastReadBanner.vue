8c1e4457538ad9c73814dd467d5d8552c6ce0055
yutisio12 <yutisiogamings@gmail.com>
2026-02-16 21:00:46 +0700

    FE: - change the theme on lastreadbanner for purple color

--- components/LastReadBanner.vue
+++ components/LastReadBanner.vue
@@ -5,12 +5,12 @@
 <template>
   <div 
     v-if="lastRead"
-    class="mb-6 p-4 border bg-yellow-100 dark:bg-yellow-950/20 dark:border-yellow-700/50 flex justify-between items-center"
+    class="mb-6 p-4 border bg-purple-200 dark:bg-purple-950/20 dark:border-purple-700/50 flex justify-between items-center"
     style="border-bottom-right-radius: 10px; border-bottom-left-radius: 10px;"
   >
     <p class="text-sm text-gray-600 dark:text-gray-300 text-semibold">Terakhir Dibaca: </p>
     <NuxtLink :to="`/surah/${lastRead.surahId}#ayat-${lastRead.ayat}`"
-      class="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600">
+      class="px-4 py-2 bg-purple-500 text-white rounded hover:bg-purple-600">
       {{ lastRead.lastRead?.surahName || lastRead.surahName }} - {{ lastRead.ayat }}
     </NuxtLink>
   </div>
