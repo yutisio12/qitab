fc5fde0b577c68bc09dedab03a6e496494b780ef
yutisio12 <yutisiogamings@gmail.com>
2026-02-22 10:49:45 +0700

    FE: - set up on defaultPage for scrolltop components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="fixed bottom-6 right-6 z-[100]">
+  <div class="fixed bottom-6 right-6 z-[100] flex gap-2">
+    <ScrollToTopButton />
     <ColorModeSelector />
   </div>
   <slot />
