258d6e4a782e505aa520fb06507d1f44af7c27a6
yutisio12 <yutisiogamings@gmail.com>
2026-03-10 08:27:33 +0700

    FE:
    - comment calling scroll and color mode components on default page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="fixed bottom-6 right-6 z-[100] flex gap-2">
-    <ScrollToTopButton />
-    <ColorModeSelector />
+    <!-- <ScrollToTopButton />
+    <ColorModeSelector /> -->
   </div>
   <slot />
 </template>
